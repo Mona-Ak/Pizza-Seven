2d4a2c90a640a7e833ee7da4ddeb24bf208be229
Mona <monaakbarpour1@gmail.com>
2023-09-16 07:40:07 +0200

    adding halal aagain

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -13,7 +13,7 @@ import menu10 from "../../public/images/salatmenu.jpg";
 import menu11 from "../../public/images/shishamenu.jpg";
 import menu12 from "../../public/images/pish-ghaza.jpg";
 import menu13 from "../../public/images/weinmenu.jpg";
-// import halal from "../../public/images/foods/Halal.jpg";
+import halal from "../../public/images/foods/Halal.jpg";
 
 
 
@@ -23,6 +23,7 @@ export const Menu = () => {
     <section className="section">
       <div className="head-menus">
         <h1 >Menu</h1>
+        <Image src={halal} width={70} height={50} /> 
       </div>
       
         <div className="menu-container">
